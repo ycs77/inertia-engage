df549939fbb6ccfb9f4d03c5ab98cd79428d998a
Lucas Yang <yangchenshin77@gmail.com>
2024-05-24 22:41:32 +0800

    Update error pages style

--- stubs/error/Error-ts.vue
+++ stubs/error/Error-ts.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="antialiased">
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-50 dark:bg-gray-900 sm:items-center sm:pt-0">
       <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
         <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-          <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+          <div class="px-4 text-lg text-gray-400 border-r border-gray-400 tracking-wider">
             {{ code }}
           </div>
 
-          <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+          <div class="ml-4 text-lg text-gray-400 uppercase tracking-wider">
             {{ message }}
           </div>
         </div>
