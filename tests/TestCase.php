40ab8cfe66641bc4257e156b87bc2f13d0aedcad
Lucas Yang <yangchenshin77@gmail.com>
2024-05-31 13:26:26 +0800

    Tweak

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,13 +3,10 @@
 namespace Inertia\Engage\Tests;
 
 use Inertia\EngageServiceProvider;
-use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    // use WithWorkbench;
-
     protected function defineEnvironment($app)
     {
         $app['view']->addLocation(__DIR__.'/stubs/views');
